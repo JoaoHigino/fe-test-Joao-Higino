8188fd3c11fe14ee3105ad448f83e8e43b4a5520
JoaoHigino <jhigino10@hotmail.com>
2023-05-03 09:22:26 +0000

    Add opening crawl planets in the movie to modal

--- src/components/movieInfo.js
+++ src/components/movieInfo.js
@@ -18,10 +18,19 @@ const MovieInfo = ({ id, close }) => {
     return <p>Error : {error.message}</p>;
   }
 
+
   return <div>
     <span onClick={close}>Close</span>
     <h2>{data.film.title}</h2>
+    <h3>Opening Crawl: <br></br>{data.film.openingCrawl}</h3>
+    <h3>Planets in the movie:<br></br>
+      {data.film.planetConnection.planets.map(film => (  
+        <li>  
+          {film.name}  
+        </li>  
+      ))}  
+    </h3>
   </div>;
-}
 
+}
 export default MovieInfo;
